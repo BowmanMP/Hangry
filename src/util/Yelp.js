4eb5dd6ad20c03f5794b39f1d99d740159c50e99
BowmanMP <bowmanmp1@gmail.com>
2021-02-18 20:25:14 -0700

    updated CORS proxy URL

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,7 +2,7 @@ const apiKey = process.env.REACT_APP_YELP_API_KEY;
 
 const Yelp = {
     searchYelp (term, location, sortBy) {
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+        return fetch(`https://corsanywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
             headers: {
                 Authorization: `Bearer ${apiKey}`
             }
